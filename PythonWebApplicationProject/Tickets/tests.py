c64268e2f8da85e10426b16484262485dc79237a
18Tarikjett <43524016+18Tarikjett@users.noreply.github.com>
2024-03-10 19:21:00 +0000

    Added unit tests for authentication and authorization for Users, added SQL injection unit test for ticket class

--- PythonWebApplicationProject/Tickets/tests.py
+++ PythonWebApplicationProject/Tickets/tests.py
@@ -13,7 +13,7 @@ class TicketTest(TestCase):
         }
 
 
-        #Post ticket is sent to the 
+        #Post ticket is sent to the server 
         response = self.client.post(reverse('create'), ticket_data, follow=True)
 
         #This checks to see if the response was successful, using the HTTPS response 200 which is a succesful response status code    
@@ -34,3 +34,14 @@ class TicketTest(TestCase):
         
         self.assertEqual(response.status_code, 200)
         
+    def test_sql_inject(self):
+        
+        #Malicious sql input to simulate an injection attack through a malicious query
+        sql_injection_input = "admin' OR '1'=1"
+        ticket_vuln = Ticket.objects.create(Title=sql_injection_input, Problem = 'test inject', Status='Critical')
+        
+        #Saves the ticket which should not raise an error or exception
+        ticket_vuln.save()
+        
+        #Verifies that the injection attack hasn't affected the database. 
+        self.assertTrue(Ticket.objects.filter(title=sql_injection_input))
--- PythonWebApplicationProject/Users/tests.py
+++ PythonWebApplicationProject/Users/tests.py
@@ -1,4 +1,7 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
 class URLTests(TestCase):
@@ -9,3 +12,24 @@ class URLTests(TestCase):
     def test_logoutpage(self):
         response = self.client.get('/logout')
         self.assertEqual(response.request, 200)
+        
+    def user_setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='RandomUser', password='testpass')
+        
+    def tearDown(self):
+        self.user.delete()
+    
+    def test_unauthorised_user(self):
+        response = self.client.get(reverse('views'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/Users/views/login')
+        
+    def test_authorised_user(self):
+        self.client.login(username='RandomUser', password='testpass')
+        
+        response = self.client.get(reverse('views'))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profile.html')
+    
